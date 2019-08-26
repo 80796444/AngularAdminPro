ac41718dc02ef31ade462b3623b47dc2c805d772
fabian.ruiz <fruiz@bitgraylabs.com>
2019-08-26 13:51:17 -0500

    Version 1 Cleaned. solution was cleaned.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+
 //Rutas
 import { APP_ROUTES } from './app.routes';
-import { PagesComponent } from './pages/pages.component';
+
+//Modules
+import { PagesModule } from './pages/pages.module';
+
+//Components
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
 
 
@@ -22,18 +20,11 @@ import { RegisterComponent } from './login/register.component';
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    PagesModule,
     APP_ROUTES
   ],
   providers: [],
