5183803c3e21388cbb18af129b73c8d515baa9ad
fabian.ruiz <fruiz@bitgraylabs.com>
2019-08-26 11:34:43 -0500

    first commit

---
+++ src/app/login/register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'register',
+  templateUrl: './register.html',
+  styleUrls: ['./login.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
