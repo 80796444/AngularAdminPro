7135baba2b4d9d68779333402b03a560a5dbdec6
Fabian Ruiz <v-fabian@schedulicity.com>
2020-02-11 10:33:23 -0500

    el tema se cambia a gusto del usuario

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-account-settings',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountSettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject (DOCUMENT) private _document) { }
 
   ngOnInit() {
+  }
+
+  cambiarTema(color: string) {
+    let ref = `assets/css/colors/${color}.css`;
+    this._document.getElementById('tema').setAttribute('href', ref);
+
   }
 
 }
