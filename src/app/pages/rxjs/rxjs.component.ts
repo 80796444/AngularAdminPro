2545f80110428973156823bae2feb8c219346514
Fabian Ruiz <v-fabian@schedulicity.com>
2020-02-24 09:51:42 -0500

    conceptos de promesas y observadores.

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,38 +1,64 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscriber, Subscription } from 'rxjs';
+import { retry, map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'rxjs.component',
   templateUrl: './rxjs.component.html',
 })
-export class RxjsComponent implements OnInit {
+export class RxjsComponent implements OnInit, OnDestroy {
+ 
+  subscripcion: Subscription;
 
   constructor() { 
-
-    let obs = new Observable( observer => {
-      let contador = 0;
-      let intervalo = setInterval(() => {
-        contador += 1;
-        observer.next(contador);
-
-        if (contador === 4) {
-          clearInterval(intervalo);
-          observer.complete();
-        }
-        if (contador ===2) {
-          observer.error('auxilio');
-        }
-
-      }, 1000)
-    });
-
-    obs.subscribe(numero => console.log('subs' + numero),
+    this.subscripcion = this.regresaObservable().pipe(retry(2))
+    .subscribe(numero => console.log('subs' + numero),
                   error => console.error('Error en el obs', error),
                   () => console.log('El observador termino!')
     );
   }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy(): void {
+  console.log('dejo la pagina');
+   this.subscripcion.unsubscribe();
+  }
+
+
+  regresaObservable(): Observable<any> {
+    return new Observable((observer: Subscriber<any>) => {
+      console.log('creo nuevo observador');
+      let contador = 0;
+      let intervalo = setInterval(() => {
+        contador += 1;
+        let salida = {
+          valor: contador
+        }
+        observer.next(salida);
+
+        if (contador === 30) {
+          clearInterval(intervalo);
+          observer.complete();
+        }
+        // if (contador === 2) {
+        //   //clearInterval(intervalo);
+        //   observer.error('auxilio');
+        // }
+
+      }, 1000)
+    }).pipe(
+      map(resp => {
+        return resp.valor
+      }), filter((valor, index) => {
+        if ((valor % 2)===1){
+          return true;
+        } else {
+          return false;
+        }
+      })
+    );
   }
 
 }
